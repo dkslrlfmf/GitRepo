9bc7fb4e61ac7f1e0873b35e7b2104be13d83709
dkslrlfmf <leesc0406@gmail.com>
2017-02-23 10:19:25 +0900

    test

--- gs-uploading-files-complete/gradlew.bat
+++ gs-uploading-files-complete/gradlew.bat
@@ -32,7 +32,8 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
+@rem set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_HOME=C:\Java\jdk1.8.0_121_64
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
